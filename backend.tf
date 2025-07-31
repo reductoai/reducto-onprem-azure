a832dd3ca5e4f057654d7aaba267dd98ed281cb9
Himanshu Neema <himanshu@reducto.ai>
2025-07-30 18:36:05 -0700

    update backend

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 #     use_cli              = true                                    
 #     use_azuread_auth     = true                                    
 #     tenant_id            = "00000000-0000-0000-0000-000000000000"
-#     storage_account_name = "reducto-tfstate-<name>"                             
+#     storage_account_name = "reductotfstate<name>"
 #     container_name       = "reducto-tfstate"                              
 #     key                  = "terraform.tfstate"                
 #   }
