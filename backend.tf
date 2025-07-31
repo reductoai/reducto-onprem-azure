dff4e32e7972032c041711f4cb2b122d80726a04
Himanshu Neema <himanshu@reducto.ai>
2025-07-30 17:26:27 -0700

    add backend for tfstate

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# terraform {
+#   backend "azurerm" {
+#     use_cli              = true                                    
+#     use_azuread_auth     = true                                    
+#     tenant_id            = "00000000-0000-0000-0000-000000000000"
+#     storage_account_name = "reducto-tfstate-<name>"                             
+#     container_name       = "reducto-tfstate"                              
+#     key                  = "terraform.tfstate"                
+#   }
+# }
